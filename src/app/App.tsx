f7cc9b429f6264ad6df22cb7bcd7271438b6b639
Marcos Peres <peres_m@mail.ru>
2023-05-15 22:03:08 +0300

    create Navbar & AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,17 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import AboutPage from "pages/AboutPage/ui/AboutPage";
-import MainPage from "pages/MainPage/ui/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage/>} />
-                </Routes>
-            </Suspense>
         </div>
     );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Modes = Record<string, boolean | string>
 
-export function classNames(cls: string, mode: Modes, additional: string[]): string {
+export function classNames(cls: string, mode: Modes = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mode)
             .filter(([className, value]) => Boolean(value))
             .map(([className]) => className)

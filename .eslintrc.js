bdd75a3e5c9b6e677f97be8962c48962a8db3eb0
Marcos Peres <peres_m@mail.ru>
2023-06-08 00:17:07 +0300

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -32,8 +33,14 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.ts', '.js', '.jsx', '.tsx'] }],
-        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['to'] }],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.ts', '.js', '.jsx', '.tsx'] },
+        ],
+        'i18next/no-literal-string': [
+            'error',
+            { markupOnly: true, ignoreAttribute: ['to'] },
+        ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'react/require-default-props': 'off',
@@ -45,6 +52,7 @@ module.exports = {
         'no-unused-vars': 'warn',
         'react/jsx-props-no-spreading': 'warn',
         'import/no-extraneous-dependencies': 'warn',
+        'max-len': ['error', { ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,

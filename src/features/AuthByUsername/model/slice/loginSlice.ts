53482f94981272eacd6f168e7c5359dc2e51356d
Marcos Peres <peres_m@mail.ru>
2023-10-07 11:43:06 +0300

    change LoginSchema import

--- src/features/AuthByUsername/model/slice/loginSlice.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { LoginSchema } from '../types/loginSchema';
+import { LoginSchema } from 'features/AuthByUsername';
 import { loginByUsername } from '../services/loginByUsername/loginByUsername';
 
 const initialState: LoginSchema = {
